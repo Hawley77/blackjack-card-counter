7b9da059bd499129af5d71a20b04d5a4e891e0e7
Hawley77 <hop_watch@hotmail.com>
2024-09-06 09:40:00 -0700

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,23 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('src/index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
